e4f632a0e58ed9ca889f419c417ec2fca41b246f
Nisarg Patel <patel321nisarg@gmail.com>
2022-06-07 00:37:59 -0400

    A3- Added and implemented activity_clicky_clicky.xml.

---
+++ app/src/main/java/com/cs5520/assignments/numad2022su_regular/ClickyClickyActivity.kt
@@ -0,0 +1,11 @@
+package com.cs5520.assignments.numad2022su_regular
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClickyClickyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clicky_clicky)
+    }
+}
--- app/src/main/java/com/cs5520/assignments/numad2022su_regular/MainActivity.kt
+++ app/src/main/java/com/cs5520/assignments/numad2022su_regular/MainActivity.kt
@@ -3,21 +3,34 @@ package com.cs5520.assignments.numad2022su_regular
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var aboutMeBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         aboutMeBtn = findViewById(R.id.about_me_btn)
-        aboutMeBtn.setOnClickListener{clickAboutMeBtn()}
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.about_me_btn) {
+            clickAboutMeBtn()
+        } else if (view?.id == R.id.clicky_clicky_btn) {
+            clickClickyClicky()
+        }
     }
 
     private fun clickAboutMeBtn() {
         val intent = Intent(this, AboutMeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun clickClickyClicky() {
+        val intent = Intent(this, ClickyClickyActivity::class.java)
         startActivity(intent)
     }
 }
